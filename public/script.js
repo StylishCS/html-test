5a4be80bae79adcff3d39d3e900eb701512a6651
StylishCS <joeshirf@gmail.com>
2023-07-28 05:49:17 +0300

    m

--- public/script.js
+++ public/script.js
@@ -4,16 +4,20 @@ const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
 
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
 socket.on('chat-message', data => {
-    appendMessage(data);
+  appendMessage(`${data.name}: ${data.message}`)
 })
 
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
+})
 
-messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    socket.emit('send-chat-message', message)
-    messageInput.value = ''
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
 })
 
 function appendMessage(message) {
