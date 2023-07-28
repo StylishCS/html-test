2b500056ea6b07a6c173dcdbd8e97acaf40ec6a7
StylishCS <joeshirf@gmail.com>
2023-07-28 05:11:15 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/', (req,res)=>{
+    res.send('test');
+})
+
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', socket =>{
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message',message);
+    })
+})
+
+
+http.listen(8080);
